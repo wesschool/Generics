623ecaa935f4c0222bc20976fb5a2cef69179602
wesschool <lyonsw@wsesu.org>
2017-03-30 11:50:05 -0400

    Update Math_Lib.h

--- Math_Lib.h
+++ Math_Lib.h
@@ -14,24 +14,24 @@ namespace Math
 
 		return temp;
 	}
-	
+
 	template <typename T>
 	T  absoluteValue(T value)
 	{
 		return (value < 0) ? -value : value;
 	}
-	
+
 	template <typename T>
-	T getBiggest(T a, T b)
+	T max(T a, T b)
 	{
 		return (a > b) ? a : b;
 	}
 
 	template <typename T>
-	T getBiggest(std::vector <T>* values)
+	T max(std::vector <T>* values)
 	{
 		T biggest = (*values)[0];
-		for (int i = 0; i < values->size(); ++i)
+		for (int i = 1; i < values->size(); ++i)
 		{
 			if ((*values)[i] > biggest)
 				biggest = (*values)[i];
@@ -41,16 +41,16 @@ namespace Math
 	}
 
 	template <typename T>
-	T getSmallest(T a, T b)
+	T min(T a, T b)
 	{
 		return (a < b) ? a : b;
 	}
 
 	template <typename T>
-	T getSmallest(std::vector <T>* values)
+	T min(std::vector <T>* values)
 	{
 		T smallest = (*values)[0];
-		for (int i = 0; i < values->size(); ++i)
+		for (int i = 1; i < values->size(); ++i)
 		{
 			if ((*values)[i] < smallest)
 				smallest = (*values)[i];
